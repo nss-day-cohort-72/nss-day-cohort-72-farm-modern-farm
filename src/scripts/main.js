fc00fb26b552e5a2667f49b49b62ef6bd4ca2589
Robert Mack <robertmack123123@gmail.com>
2024-08-08 09:18:36 -0500

    made seed modules and functions to create each seed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
 import { createPlan } from "./plan"
+import { createAsparagus } from "./seeds/asparagus.js";
+import { createCorn } from "./seeds/corn.js";
+import { createPotato } from "./seeds/potato.js";
+import { createSoybean } from "./seeds/soybean.js";
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js";
 
 const yearlyPlan = createPlan();
-
 console.log("Welcome to the main module")
-
 
 
