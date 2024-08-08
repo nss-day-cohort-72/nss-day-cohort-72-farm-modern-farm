7ff4dc012ca9b199e23fb878e017d565844ef9a7
Mike Sheets <wmichaelsheets@gmx.com>
2024-08-08 11:29:15 -0400

    Adding harvester module

---
+++ src/scripts/harvester.js
@@ -0,0 +1,20 @@
+import { usePlants } from "./field.js"
+
+export const harvestPlants = (plants) => {      //from Field module usePlant() that returns a copy of the array of plants
+    const seeds = []
+  
+    for (const plant of plants) {
+        if (plant.type === "Corn") {
+            const numberOfSeedsToAdd = Math.floor(plant.output /2)
+            for (let i = 0; i < numberOfSeedsToAdd; i++) {
+                seeds.push(plant);
+            }
+        }else {
+            for (let i = 0; i <plant.output; i++){
+                seeds.push(plant)
+            }
+        }
+    }
+
+    return seeds
+}
