4910034365c5f6456ac9087e06d5adf2429b0335
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-08-08 11:04:08 -0500

    update main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,17 @@
-import { createPlan } from "./plan.js"
-import { addPlant, usePlants } from "./field.js";
-import { createAsparagus } from "./seeds/asparagus.js";
-import { createCorn } from "./seeds/corn.js";
-import { createPotato } from "./seeds/potato.js";
-import { createSoybean } from "./seeds/soybean.js";
-import { createSunflower } from "./seeds/sunflower.js"
-import { createWheat } from "./seeds/wheat.js";
+import { createPlan } from "./plan.js";
+import { usePlants } from "./field.js";
+import { plantSeeds } from "./tractor.js";
+import { harvestPlants} from "./harvester.js";
+
 
 
 const yearlyPlan = createPlan();
 console.log("Welcome to the main module")
 
-const seed = [{
-    type: "corn",
-    height: 3,
-    output: 4,
-},{
-    type:"corn",
-    height: 3,
-    output: 4
-}];
+plantSeeds(yearlyPlan);
 
-addPlant(seed);
 
 const usePlantsVariable= usePlants();
-console.log(usePlantsVariable);
+
+const harvestPlants = harvestPlants(usePlantsVariable);
 
