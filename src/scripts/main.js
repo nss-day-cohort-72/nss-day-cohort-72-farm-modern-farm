0bd0be59d598d66498be64332d73fe7fe3e02065
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-08-08 16:29:54 -0500

    showing quantity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ import { harvestPlants} from "./harvester.js";
 import { Catalog } from "./catalog.js";
 
 
+
 const yearlyPlan = createPlan();
 console.log("Welcome to the main module")
 
@@ -15,7 +16,28 @@ const usePlantsVariable= usePlants();
 
 const harvestedPlants = harvestPlants(usePlantsVariable).sort((a,b)=> 
     a.type.localeCompare(b.type));
+let filteredArray = []
+const plantCounts = harvestedPlants.reduce((prev,curr) => {
+    
+    if (curr.type !== prev[prev.length -1].type) {
+        curr.quantity = 1
+        filteredArray.push(curr)
+
+        
+    }else{
+        filteredArray.forEach(plant =>{
+            if (plant.type === curr.type) {
+                plant.quantity++;
+            }
+        })
+    }
+    return filteredArray;
+},[{type:"none"}]);
 
 
 
-Catalog(harvestedPlants);
+
+
+
+
+Catalog(plantCounts);
